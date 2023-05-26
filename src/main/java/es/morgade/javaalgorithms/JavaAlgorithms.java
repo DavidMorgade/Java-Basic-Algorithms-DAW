6ab6cced0e2681a9466cacf529e61b1332e6be72
¨DavidMorgade¨ <davidmorgadegil@gmail.com>
2023-05-26 17:06:56 +0200

    Primer commit

---
+++ src/main/java/es/morgade/javaalgorithms/JavaAlgorithms.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.morgade.javaalgorithms;
+
+/**
+ *
+ * @author morgade
+ */
+public class JavaAlgorithms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
